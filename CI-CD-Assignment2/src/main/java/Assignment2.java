f5f818a10d411c61da4e16efd14b6d99f0f3f032
Prashasti Gupta <princess.prashastigupta@gmail.com>
2020-11-20 18:50:20 +0530

    First Commit

---
+++ CI-CD-Assignment2/src/main/java/Assignment2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Assignment2 {
+public static void main(String[] args)
+{
+	Scanner sc = new Scanner(System.in);
+	int input = sc.nextInt();
+	int square=0;
+	square = input*input;
+	System.out.println(square);
+}
+}
