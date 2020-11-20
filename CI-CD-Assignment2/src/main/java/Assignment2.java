4149e09ce5cf5760f68d5f4ce952266334e89af1
Prashasti Gupta <princess.prashastigupta@gmail.com>
2020-11-20 18:53:37 +0530

    Second Commit

--- CI-CD-Assignment2/src/main/java/Assignment2.java
+++ CI-CD-Assignment2/src/main/java/Assignment2.java
@@ -4,8 +4,10 @@ public static void main(String[] args)
 {
 	Scanner sc = new Scanner(System.in);
 	int input = sc.nextInt();
-	int square=0;
+	int square=0, cube=0;
 	square = input*input;
+	cube = input*input*input;
 	System.out.println(square);
+	System.out.println(cube);
 }
 }
